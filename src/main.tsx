235269a3d0437e414f932ec031ff09c1f1ba9478
joaopaulosf <jpforcode@gmail.com>
2023-06-20 18:21:20 -0300

    :lipstick: Add css variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./assets/styles/settings/colors.css";
+import "./assets/styles/settings/size.css";
 import "./assets/styles/generic/reset.css";
 import "./assets/styles/elements/base.css";
 import "./assets/styles/mediaqueries/styles.css";
